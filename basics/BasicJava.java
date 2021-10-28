732bd71c1a5c60f2aa40a5008f3239ac1587433f
k15arai <kenji.arai@gmail.com>
2021-10-27 23:47:38 -0700

    still a work in progress

---
+++ basics/BasicJava.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+
+public class BasicJava {
+    // print 1-255
+    public void printTo255() {
+        int i = 1;
+        while (i < 256) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    // print odd numbers between 1-255
+    public void printOddNumsTo255() {
+        int i = 1;
+        while (i < 256) {
+            if (i%2 == 1) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+
+    // sigma of 255. aka sum to 255
+
+    public int returnSumOfNumsTo255() {
+        int i = 1;
+        int sum = 0;
+        while (i < 256) {
+            sum += i;
+            i++;
+        }
+        return sum;
+    }
+
+    // iterating through an array
+    public void iterateThroughArray() {
+        int[] practiceArray = {1,3,5,7,9,13};
+        for (int i=0; i < practiceArray.length; i++) {
+            System.out.println(practiceArray[i]);
+        }
+    }
+
+    // find max
+    public int findMax(int [] array) {
+        int max = array[0];
+        for (int i=1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    // array with odd numbers
+    public ArrayList<Integer> arrayWithOddNumbers(int topNumber) {
+        ArrayList<Integer> oddNumList = new ArrayList<Integer>();
+        for (int i=0; i <= topNumber; i++) {
+            if (i%2 == 1) {
+                oddNumList.add(i);
+            }
+        }
+        return oddNumList;
+    }
+
+    // NINJA BONUS SECTION
+
+    // get average
+    public float getAverage(int[] array) {
+        float sum = 0;
+        for (int i=0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum);
+        System.out.println(array.length);
+        return (sum / array.length);
+    }
+
+    // greater than Y
+    public int greaterThanY(int[] array, int yNumber) {
+        int count = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] > yNumber) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // square the values
+    public ArrayList<Integer> squareArray(int[] array) {
+        ArrayList<Integer> squaredList = new ArrayList<Integer>();
+        for (int i=0; i <= array.length; i++) {
+            int squared = array[i] * array[i];
+            squaredList.add(squared);
+        }
+        return squaredList;
+    }
+
+    // eliminate negative values
+
+    // SENSEI BONUS SECTION
+
+    // Max, Min, and Average
+
+    // Shifting the values in the Array
+
+
+}
---
+++ basics/BasicJavaTest.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class BasicJavaTest {
+    public static void main(String[] args) {
+        
+        // Make an instance of BasicJava to access all the methods to test
+        BasicJava basicJavaBot = new BasicJava();
+
+        // Run some tests of the methods, providing necessary data
+        
+        // print numbers from 1 to 255
+        // basicJavaBot.printTo255();
+
+        // print odd numbers from 1 to 255
+        // basicJavaBot.printOddNumsTo255();
+
+        // Sum of numbers to 255
+        // int sumTo255 = basicJavaBot.returnSumOfNumsTo255();
+        // System.out.println(sumTo255);
+
+        // iterate through an array
+        // basicJavaBot.iterateThroughArray();
+
+        // find max number of an array that is passed into a function
+        // int[] myArray_max = {-51, -2, -6, 74, 10, 150};
+        // int maxfromArray = basicJavaBot.findMax(myArray_max);
+        // System.out.println(maxfromArray);
+
+        // create array with odd numbers between 1 to 255
+        // ArrayList<Integer> myArray_odd = new ArrayList<Integer>();
+        // myArray_odd = basicJavaBot.arrayWithOddNumbers(255); // can change the parameter to change array output
+        // System.out.println(myArray_odd);
+
+        // NINJA BONUS
+
+        // get average
+        // int[] arrayForAverage = {-1,2,-3,4,-5,6,7,20};
+        // float average = basicJavaBot.getAverage(arrayForAverage);
+        // System.out.println(average);
+
+        // greater than Y
+        // int[] arrayToCheck = {2,4,5,6,7,9,10,11,12};
+        // int Y = 8;
+        // int numsGreaterThanY = basicJavaBot.greaterThanY(arrayToCheck, Y);
+        // System.out.println(numsGreaterThanY);
+
+        // square values
+        int[] initialArray = {1,2,3,4,5,6,7};
+        ArrayList<Integer> myArraySquared = new ArrayList<Integer>();
+        myArraySquared = basicJavaBot.squareArray(initialArray);
+        System.out.println(myArraySquared);
+
+
+
+    }
+}
